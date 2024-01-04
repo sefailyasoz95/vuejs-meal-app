399a8d8d09cfce449affb3e4546815a2356edd6c
Sefa İlyas Öz <sefailyas1455@gmail.com>
2024-01-04 23:47:50 +0300

    clean up

--- src/store/mutations.ts
+++ src/store/mutations.ts
@@ -1,22 +1,7 @@
-// function setSearchedMeals(state: any, meals: any) {
-// 	state.searchedMeals = meals;
-// }
-// function setCategories(state: any, categories: any) {
-// 	state.categories = categories;
-// }
-// function setErrorMessage(state: any, message: string) {
-// 	state.errorMessage = message;
-// }
-
 function setState(state: any, payload: { key: string; value: any }) {
-	console.log("payload: ", payload);
-
 	state[payload.key] = payload.value;
 }
 
 export default {
-	// setSearchedMeals,
-	// setErrorMessage,
-	// setCategories,
 	setState,
 };
